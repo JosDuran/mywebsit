4c4eae6aeece469f2147467f95b4f851c7a4b21b
josduran@gmail.com <josduran@gmail.com>
2023-10-11 00:18:17 -0500

    prueba

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,18 @@
-import Necesidades from '@/components/necesidades'
-import Image from 'next/image'
-
 
 export default function Home() {
   return (
-   <Necesidades/>
+
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="max-w-md w-full mx-auto p-6 bg-white shadow-md rounded-lg">
+
+
+    <div className= "bg-neutral text-neutral-content">
+      <p>
+        Nosotros desarrollamos <span className="text-xl font-semibold text-blue-500">aplicaciones web</span> Un aplicativo web, es un programa que se ejecuta en un servidor, y ud puede interactuar con dicho programa a travez de un navegador web, como es sabido hoy en dia tanto pcs, laptops y tablets cuentan con navegadores web, un ejemplo clasico es el chrome, aunque existen otros como mozilla o edge. Para ver los distintos tipos de aplicaciones web que existen dele click aqui </p>
+    </div>
+
+    </div>
+    </div>
+  
   )
 }
